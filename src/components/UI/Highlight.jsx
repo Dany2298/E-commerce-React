9956ef69cb811a2f1d03c1fb3c678da3ac12c06f
Dany2298 <dany22986@gmail.com>
2023-05-27 17:18:28 +0100

    added dynamic components

--- src/components/UI/Highlight.jsx
+++ src/components/UI/Highlight.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-function Highlight({ icon }) {
+function Highlight({ icon, title, para }) {
   return (
     <div className="highlight">
       <div className="highlight__img">{icon}</div>
-      <h3 className="highlight__subtitle">Easy and Quick</h3>
-      <p className="highlight__para">
-        Get access to the book you purchased online instantly.
-      </p>
+      <h3 className="highlight__subtitle">{title}</h3>
+      <p className="highlight__para">{para}</p>
     </div>
   );
 }
