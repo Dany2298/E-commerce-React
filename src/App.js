1182c2f3fcc1e984bae6617e3a905177b30290ef
Dany2298 <dany22986@gmail.com>
2023-05-29 18:40:33 +0100

    sort filter done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Nav from "./components/Nav.jsx";
-
 import "./index.css";
-
 import Footer from "./components/Footer.jsx";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/pages/Home.jsx";
+import Books from "./components/pages/Books.jsx";
+import { books } from "./data.js";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <div className="App">
         <Nav />
         <Route path="/" exact component={Home} />
+        <Route path="/books" render={() => <Books books={books} />} />
         <Footer />
       </div>
     </Router>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Landing from "../Landing";
-import Highlight from "../UI/Highlight";
+import Highlight from "../Highlights";
 import Featured from "../Featured";
 import Discounted from "../Discounted";
 import Explore from "../Explore";
