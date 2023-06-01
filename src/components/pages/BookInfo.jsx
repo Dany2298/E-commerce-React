4c7b9aa459c8a314d6cfe7f8068d29f6a5bc0846
Dany2298 <dany22986@gmail.com>
2023-06-01 14:11:11 +0100

    BookInfo Sorting

--- src/components/pages/BookInfo.jsx
+++ src/components/pages/BookInfo.jsx
@@ -1,10 +1,15 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import Rating from "../UI/Rating";
 import Price from "../UI/Price";
+import Book from "../UI/Book";
 
 function BookInfo({ books }) {
+  const { id } = useParams();
+
+  const book = books.find((book) => +book.id === +id);
+
   return (
     <div id="books__body">
       <main id="books__main">
@@ -20,22 +25,19 @@ function BookInfo({ books }) {
             </div>
             <div className="book__selected">
               <figure className="book__selected--figure">
-                <img
-                  src="https://covers.openlibrary.org/b/id/8091016-L.jpg"
-                  alt=""
-                  className="book__selected--img"
-                />
+                <img src={book.url} alt="" className="book__selected--img" />
               </figure>
               <div className="book__selected--description">
-                <h2 className="book__selected--title">
-                  Crack the Coding Interview
-                </h2>
-                <Rating rating="4.5" />
+                <h2 className="book__selected--title">{book.title}</h2>
+                <Rating rating={book.rating} />
                 <div className="book__selected--price">
-                  <Price salePrice={10} originalPrice={20} />
+                  <Price
+                    salePrice={book.salePrice}
+                    originalPrice={book.originalPrice}
+                  />
                 </div>
                 <div className="book__summary">
-                  <div className="book__summary--title">Summary</div>
+                  <h3 className="book__summary--title">Summary</h3>
                 </div>
                 <p className="book__summary--para">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -51,8 +53,8 @@ function BookInfo({ books }) {
                   ipsam eveniet alias mollitia distinctio, tempore, repudiandae
                   incidunt!
                 </p>
+                <button className="btn">Add to Cart</button>
               </div>
-              <button className="btn">Add to Cart</button>
             </div>
           </div>
         </div>
@@ -61,6 +63,14 @@ function BookInfo({ books }) {
           <div className="row">
             <div className="books__selected--top">
               <h2 className="book__selected--title--top">Recommended Books</h2>
+            </div>
+            <div className="books">
+              {books
+                .filter((book) => book.rating === 5 && book.id !== id)
+                .slice(0, 4)
+                .map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
             </div>
           </div>
         </div>
